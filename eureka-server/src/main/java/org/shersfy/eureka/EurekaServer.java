fe48e03b2de9dbd9a75779b014631d049fcf4316
pengyang <pengyang@cmhi.chinamobile.com>
2018-07-14 14:40:10 +0800

    first commit

---
+++ eureka-server/src/main/java/org/shersfy/eureka/EurekaServer.java
@@ -0,0 +1,18 @@
+package org.shersfy.eureka;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(EurekaServer.class);
+        app.setBannerMode(Mode.OFF);
+        app.run(args);
+    }
+
+}
---
+++ eureka-server/src/test/java/org/shersfy/eureka/EurekaTest.java
@@ -0,0 +1,12 @@
+package org.shersfy.eureka;
+
+import org.junit.Test;
+
+public class EurekaTest {
+	
+	@Test
+	public void test01() {
+		
+	}
+
+}
