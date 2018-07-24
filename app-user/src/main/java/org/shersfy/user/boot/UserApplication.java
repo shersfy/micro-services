6e0c77ee0f0f18023a984da5cb82f689df8baf28
pengyang <pengyang@cmhi.chinamobile.com>
2018-07-25 00:34:20 +0800

    discover client

--- app-user/src/main/java/org/shersfy/user/boot/UserApplication.java
+++ app-user/src/main/java/org/shersfy/user/boot/UserApplication.java
@@ -4,8 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @ComponentScan("org.shersfy.user")
 @MapperScan("org.shersfy.user.mapper")
 @SpringBootApplication
